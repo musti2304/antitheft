3886a7d38cca42d1aabe69b83deb85d98cda26d0
Mustafa Yousef <mustafa.yousef@cologne-intelligence.de>
2017-11-10 20:45:29 +0100

    Added audio playback

--- app/src/main/java/com/yousef/mustafa/antitheft/PowerConnectionReceiver.java
+++ app/src/main/java/com/yousef/mustafa/antitheft/PowerConnectionReceiver.java
@@ -3,13 +3,12 @@ package com.yousef.mustafa.antitheft;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.widget.Toast;
 
-/**
- * Created by myousef on 10.11.17.
- */
-
 public class PowerConnectionReceiver extends BroadcastReceiver {
+
+    MediaPlayer mediaPlayer = new MediaPlayer();
 
     // Determine whether the AC is disconnected or not
     private boolean POWER_DISCONNECTED = true;
@@ -21,9 +20,17 @@ public class PowerConnectionReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         if (POWER_DISCONNECTED) {
+            mediaPlayer = MediaPlayer.create(context, R.raw.alarm_2);
             Toast.makeText(context, "Alarm started", Toast.LENGTH_SHORT).show();
+            mediaPlayer.start();
+            mediaPlayer.setLooping(true);
         } else {
             Toast.makeText(context, "Alarm stopped", Toast.LENGTH_SHORT).show();
+            if (mediaPlayer.isPlaying())
+            {
+                mediaPlayer.reset();
+                mediaPlayer.release();
+            }
         }
     }
 
