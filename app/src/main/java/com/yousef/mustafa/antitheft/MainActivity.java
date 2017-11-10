7788e04468c90cd6c3c311856c5c6c7cb7f53f50
Mustafa Yousef <mustafa.yousef@cologne-intelligence.de>
2017-11-10 17:54:22 +0100

    BroadcastReceiver implemented

--- app/src/main/java/com/yousef/mustafa/antitheft/MainActivity.java
+++ app/src/main/java/com/yousef/mustafa/antitheft/MainActivity.java
@@ -4,6 +4,7 @@ package com.yousef.mustafa.antitheft;
 import android.content.BroadcastReceiver;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.net.ConnectivityManager;
 import android.os.BatteryManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,41 +17,22 @@ import android.widget.ToggleButton;
 public class MainActivity extends AppCompatActivity {
 
     Button startServiceButton;
-    Intent batteryStatus = new Intent();
-    IntentFilter batteryChangedIntentFilter;
-    PowerConnectionReceiver powerConnectionReceiver;
-
-    /*
-    // Get status (charging or full)
-    int status = batteryStatus.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
-
-    // Is the device currently loading battery?
-    boolean isCharging = status == BatteryManager.BATTERY_STATUS_FULL;
-
-    // Get status (AC or USB)
-    int chargePlug = batteryStatus.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1);
-
-    // Is the device loading with AC or USB?
-    boolean usbCharge = chargePlug == BatteryManager.BATTERY_PLUGGED_USB;
-    boolean acCharge = chargePlug == BatteryManager.BATTERY_PLUGGED_AC;
-    */
+    BroadcastReceiver broadcastReceiver;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         startServiceButton = (ToggleButton) findViewById(R.id.startServiceButton);
-        batteryChangedIntentFilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
-        powerConnectionReceiver = new PowerConnectionReceiver();
 
-        batteryStatus = getApplicationContext().registerReceiver(null, batteryChangedIntentFilter);
+        broadcastReceiver = new PowerConnectionReceiver();
 
 
         startServiceButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (startServiceButton.isActivated()) {
+                    unregisterReceiver(broadcastReceiver);
                     Toast.makeText(MainActivity.this, "Service stopped", Toast.LENGTH_SHORT).show();
                     Log.i("button disabled", "Button is disabled, monitoring stopped");
                     startServiceButton.setActivated(false);
@@ -61,13 +43,14 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    // Replace
     private void monitorBatteryChanges() {
+        IntentFilter airplaneModeIntentFilter = new IntentFilter("android.intent.action.AIRPLANE_MODE");
+        registerReceiver(broadcastReceiver, airplaneModeIntentFilter);
 
         Log.i("button enabled", "Button is enabled, monitoring started");
         Toast.makeText(MainActivity.this, "Service started", Toast.LENGTH_LONG).show();
-
         startServiceButton.setActivated(true);
+
         playAlarmIfChargerWasRemoved();
 
     }
--- app/src/main/java/com/yousef/mustafa/antitheft/PowerConnectionReceiver.java
+++ app/src/main/java/com/yousef/mustafa/antitheft/PowerConnectionReceiver.java
@@ -3,8 +3,7 @@ package com.yousef.mustafa.antitheft;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
-import android.os.BatteryManager;
+import android.widget.Toast;
 
 /**
  * Created by myousef on 10.11.17.
@@ -12,26 +11,10 @@ import android.os.BatteryManager;
 
 public class PowerConnectionReceiver extends BroadcastReceiver {
 
-    Intent batteryStatus = new Intent();
-    IntentFilter batteryChangedIntentFilter;
-
-
     @Override
     public void onReceive(Context context, Intent intent) {
-        batteryStatus = context.registerReceiver(this, batteryChangedIntentFilter);
 
-        int status = intent.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
-        boolean isCharging = status == BatteryManager.BATTERY_STATUS_CHARGING ||
-                status == BatteryManager.BATTERY_STATUS_FULL;
-
-        int chargePlug = intent.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1);
-        boolean usbCharge = chargePlug == BatteryManager.BATTERY_PLUGGED_USB;
-        boolean acCharge = chargePlug == BatteryManager.BATTERY_PLUGGED_AC;
-    }
-
-    public boolean checkIfPowerIsConnected() {
-
-        return true;
+        Toast.makeText(context, "Flugmodus aktiviert", Toast.LENGTH_LONG).show();
 
     }
 }
