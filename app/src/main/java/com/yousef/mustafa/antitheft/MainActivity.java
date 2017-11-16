fa754d8eee9543f7699feb3642991c7db6910b03
Mustafa Yousef <mustafa.yousef@cologne-intelligence.de>
2017-11-16 15:32:50 +0100

    Optimize imports

--- app/src/main/java/com/yousef/mustafa/antitheft/MainActivity.java
+++ app/src/main/java/com/yousef/mustafa/antitheft/MainActivity.java
@@ -2,18 +2,11 @@ package com.yousef.mustafa.antitheft;
 
 
 import android.content.BroadcastReceiver;
-import android.content.Intent;
 import android.content.IntentFilter;
-import android.graphics.Color;
-import android.graphics.drawable.AnimationDrawable;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.os.Handler;
-import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.RelativeLayout;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
--- app/src/main/java/com/yousef/mustafa/antitheft/PowerConnectionBroadcastReceiver.java
+++ app/src/main/java/com/yousef/mustafa/antitheft/PowerConnectionBroadcastReceiver.java
@@ -3,15 +3,8 @@ package com.yousef.mustafa.antitheft;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.drawable.AnimationDrawable;
-import android.graphics.drawable.ColorDrawable;
 import android.media.MediaPlayer;
-import android.view.View;
-import android.widget.RelativeLayout;
 import android.widget.Toast;
-
-import java.util.zip.Inflater;
 
 public class PowerConnectionBroadcastReceiver extends BroadcastReceiver {
 
